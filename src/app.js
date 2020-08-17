ba9dc5ea31b4d5af120ad8e130e8187ec13d34a5
jgabriel1 <josegabriel@det.ufc.br>
2020-08-17 17:47:18 -0300

    Finished all routes.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 
-// const { uuid } = require("uuidv4")
+const { uuid } = require("uuidv4")
 
 const app = express()
 
@@ -11,23 +11,82 @@ app.use(cors())
 const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 })
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const newRepository = {
+    id: uuid(),
+    likes: 0,
+    title,
+    url,
+    techs,
+  }
+
+  repositories.push(newRepository)
+
+  return response.status(201).json(newRepository)
 })
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository doesn\'t exist!' })
+  }
+
+  const currentRepository = repositories[repoIndex]
+
+  const editedRepository = {
+    ...currentRepository,
+    title: title && title,
+    url: url && url,
+    techs: techs && techs,
+  }
+
+  repositories[repoIndex] = editedRepository
+
+  return response.json(editedRepository)
 })
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository doesn\'t exist!' })
+  }
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository doesn\'t exist!' })
+  }
+
+  const currentRepository = repositories[repoIndex]
+
+  const editedRepository = {
+    ...currentRepository,
+    likes: currentRepository.likes + 1,
+  }
+
+  repositories[repoIndex] = editedRepository
+
+  return response.json(editedRepository)
 })
 
 module.exports = app
